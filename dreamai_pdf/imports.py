13e99f58f20fd565eef7f2a0e20e29a2e6f77930
Hamza Farhan <thehamza96@gmail.com>
2023-05-29 20:19:30 +0500

    0.1.2

--- dreamai_pdf/imports.py
+++ dreamai_pdf/imports.py
@@ -4,3 +4,10 @@ from dreamai_dl.imports import *
 from pypdf import PdfReader
 from setfit import SetFitModel
 from sentence_transformers import SentenceTransformer
+from transformers import (
+    AutoTokenizer,
+    AutoModel,
+    AutoModelForSequenceClassification,
+    AutoModelForTokenClassification,
+    pipeline,
+)
